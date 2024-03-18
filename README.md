fac868ddb59e9ca331f42e9e0597847b73853e66
MiccWan <michaelwan0906@gmail.com>
2024-03-18 11:57:48 +0800

    Remove top layer of structure

--- README.md
+++ README.md
@@ -1,26 +1,46 @@
 <p align="center"><img src="https://dydx.exchange/icon.svg?" width="256" /></p>
 
-<h1 align="center">dYdX Chain Clients</h1>
+<h1 align="center">dYdX Chain Client for Javascript</h1>
 
 <div align="center">
   <a href='https://www.npmjs.com/package/@dydxprotocol/v4-client-js'>
     <img src='https://img.shields.io/npm/v/@dydxprotocol/v4-client-js.svg' alt='npm'/>
   </a>
-    <a href='https://pypi.org/project/v4-client-py'>
-    <img src='https://img.shields.io/pypi/v/v4-client-py.svg' alt='PyPI'/>
-  </a>
-  <a href='https://github.com/dydxprotocol/v4-clients/blob/main/LICENSE'>
+  <a href='https://github.com/dydxprotocol/v4-clients/blob/main/v4-client-js/LICENSE'>
     <img src='https://img.shields.io/badge/License-AGPL_v3-blue.svg' alt='License' />
   </a>
 </div>
 
-## v4-client-js
-The dYdX Chain Client Typescript client is used for placing transactions and querying the dYdX chain.
+The v4-Client Typescript client is used for placing transactions and querying the dYdX chain.
 
-# Third-party Clients
+## Development
 
-[C++ Client](https://github.com/asnefedovv/dydx-v4-client-cpp)
+`v4-client-js` uses node `v18` for development. You can use `nvm` to manage different versions of node.
 
-[Python Client](https://github.com/kaloureyes3/v4-clients/tree/main/v4-client-py)
+```
+nvm install
+nvm use
+nvm alias default $(nvm version) # optional
+```
 
-By clicking the above links to third-party clients, you will leave the dYdX Trading Inc. (“dYdX”) GitHub repository and join repositories made available by third parties, which are independent from and unaffiliated with dYdX. dYdX is not responsible for any action taken or content on third-party repositories.
+You can run the following commands to ensure that you are running the correct `node` and `npm` versions.
+```
+node -v # expected: v18.x.x (should match .nvmrc)
+npm -v  # expected: 8.x.x
+```
+
+## Single-JS for mobile apps
+
+Mobile apps needs to load JS as a single JS file. To build, run
+```
+npm run webpack
+```
+
+The file is generated in __native__/__ios__/v4-native-client.js
+Pending: Different configurations may be needed to generate JS for Android app
+
+## Release
+
+Using the `npm version` command will update the appropriate version tags within the package locks and also will add a git tag with the version number..
+For example `npm version minor` will perform the necessary changes for a minor version release. After the change is merged, a GitHub action will
+[publish](https://github.com/dydxprotocol/v4-clients/blob/master/.github/workflows/js-publish.yml) the new release.
---
+++ main.ts
@@ -0,0 +1,87 @@
+import 'dotenv/config';
+import Long from 'long';
+import { SubaccountClient } from './src';
+import { CompositeClient } from './src/clients/composite-client';
+import { Network, BECH32_PREFIX, } from './src/clients/constants';
+import LocalWallet from './src/clients/modules/local-wallet';
+
+const Address = process.env.ADDRESS;
+const Mnemonic = process.env.MNEMONIC;
+const FromSubaccountId = 1;
+const TradingSubIds = [20, 21];
+const USDCAssetId = 0;
+const USDCDenom = 'ibc/8E27BA2D5493AF5636760E354E46004562C46AB7EC0CC4C1CA14E9E20E2545B5';
+
+const client = await CompositeClient.connect(Network.mainnet());
+const wallet = await LocalWallet.fromMnemonic(Mnemonic, BECH32_PREFIX);
+const subaccountClients: SubaccountClient[] = [];
+
+function log(...args) {
+  console.log(`[${new Date().toLocaleString('sv-SE')}]`, ...args);
+}
+
+function getSubaccountClient(id: number) {
+  if (!subaccountClients[id]) {
+    subaccountClients[id] = new SubaccountClient(wallet, id);
+  }
+  return subaccountClients[id];
+}
+
+async function getSubBalance(toId: number) {
+  const balance = await client.indexerClient.account.getSubaccount(Address, toId);
+  return balance.subaccount.equity;
+}
+
+async function getMainBalance(address: string, denom: string) {
+  const balance = await client.validatorClient.get.getAccountBalance(address, denom);
+  return (balance ? parseInt(balance.amount, 10) : 0) / 1e6;
+}
+
+async function transfer(fromId: number, toId: number, amount: number) {
+  log(`Transferring ${amount} USDC from ${fromId} to ${toId}`);
+  await client.validatorClient.post.transfer(
+    getSubaccountClient(fromId), wallet.address!, toId, USDCAssetId, new Long(amount * 1e6));
+}
+
+async function withdraw(fromId: number, amount: number) {
+  await client.validatorClient.post.withdraw(getSubaccountClient(fromId), USDCAssetId, new Long(amount * 1e6), Address);
+}
+
+async function maintainTradingSub(toId: number) {
+  log(`Checking subaccount ${toId}`);
+  const balance = await getSubBalance(toId);
+  log(`Subaccount ${toId} has balance ${balance}`);
+  if (balance < 500) {
+    log(`Subaccount ${toId} has balance ${balance} < 500, transferring...`);
+    await transfer(FromSubaccountId, toId, 1000);
+  }
+  else if (balance < 1000) {
+    log(`Subaccount ${toId} has balance ${balance} < 1000, transferring...`);
+    await transfer(FromSubaccountId, toId, 500);
+  }
+}
+
+async function maintainMain() {
+  log(`Checking main account`);
+  const balance = await getMainBalance(Address, USDCDenom);
+  log(`Main account has balance ${balance}`);
+  if (balance < 1) {
+    log(`Main account has balance ${balance} < 1, withdrawing...`);
+    await withdraw(FromSubaccountId, 1);
+  }
+}
+
+function main() {
+  log(`Start checking...`);
+  maintainMain().catch(console.error);
+
+  for (const toId of TradingSubIds) {
+    maintainTradingSub(toId).catch(console.error);
+  }
+}
+
+log(`Start monitoring for subaccounts ${TradingSubIds}`);
+main();
+setInterval(() => {
+  main();
+}, 30 * 1000);
