0edd58cc059f2d63fb36fbccea368187ad91573a
MiccWan <michaelwan0906@gmail.com>
2024-03-18 11:04:12 +0800

    Add withdraw

--- v4-client-js/main.ts
+++ v4-client-js/main.ts
@@ -8,6 +8,7 @@ const Address = 'dydx1wz7a2ehjq0n6q7fpqkakp9928kjjhh6wzskhhe';
 const Mnemonic = 'peasant page unveil bunker oil wire general marine march shine mother height regret case evidence';
 const FromSubaccountId = 1;
 const ToSubaccountIds = [2, 3];
+const USDCAssetId = 0;
 
 const client = await CompositeClient.connect(Network.mainnet());
 const wallet = await LocalWallet.fromMnemonic(Mnemonic, BECH32_PREFIX);
@@ -17,25 +18,27 @@ function log(...args) {
   console.log(`[${new Date().toLocaleString('sv-SE')}]`, ...args);
 }
 
-function getSubaccountClient(subaccountId) {
-  if (!subaccountClients[subaccountId]) {
-    subaccountClients[subaccountId] = new SubaccountClient(wallet, subaccountId);
+function getSubaccountClient(id: number) {
+  if (!subaccountClients[id]) {
+    subaccountClients[id] = new SubaccountClient(wallet, id);
   }
-  return subaccountClients[subaccountId];
+  return subaccountClients[id];
 }
 
-async function getBalance(toId) {
+async function getBalance(toId: number) {
   const balance = await client.indexerClient.account.getSubaccount(Address, toId);
   return balance.subaccount.assetPositions.USDC.size;
 }
 
-async function transfer(fromId, toId, amount) {
-  // 0 for USDC
-  const assetId = 0;
+async function transfer(fromId: number, toId: number, amount: number) {
   log(`Transferring ${amount} USDC from ${fromId} to ${toId}`);
   return;
   await client.validatorClient.post.transfer(
-    getSubaccountClient(fromId), wallet.address!, toId, assetId, new Long(amount * 1e6));
+    getSubaccountClient(fromId), wallet.address!, toId, USDCAssetId, new Long(amount * 1e6));
+}
+
+async function withdraw(fromId: number, amount: number) {
+  await client.validatorClient.post.withdraw(getSubaccountClient(fromId), USDCAssetId, new Long(amount * 1e6), Address);
 }
 
 async function run(toId) {
